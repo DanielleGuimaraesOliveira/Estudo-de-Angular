a7c2b87f8dfcfc8646302f99e07c657e8950ce0e
Danielle Guimarães <devdanielleguimaraesoliveira@gmail.com>
2025-01-21 14:38:47 -0300

    aprendendo sobre Output

--- Sessão-4-Fundamentos-Angular-2/segundo-projeto-angular/src/app/card-btn/card-btn.component.ts
+++ Sessão-4-Fundamentos-Angular-2/segundo-projeto-angular/src/app/card-btn/card-btn.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-card-btn',
   standalone: false,
-  
+
   templateUrl: './card-btn.component.html',
   styleUrl: './card-btn.component.scss'
 })
 export class CardBtnComponent {
+  contador = 0;
 
+  @Output() btnClickEmmiter = new EventEmitter <number> (); // aqui é  basicamente estou criando uma propriedade para o componente filho que é evento de emissão e estou dando um nome para ele para aciona-lo quando precisar, e tipando o que será emitido do filho para o pai.
+
+  onClick(){
+    this.contador = this.contador+1;
+
+    this.btnClickEmmiter.emit(this.contador);
+  }
 }
--- Sessão-4-Fundamentos-Angular-2/segundo-projeto-angular/src/app/card/card.component.ts
+++ Sessão-4-Fundamentos-Angular-2/segundo-projeto-angular/src/app/card/card.component.ts
@@ -8,8 +8,12 @@ import { Component, Input } from '@angular/core';
   styleUrl: './card.component.scss'
 })
 export class CardComponent {
+  valueEmitted = 0;
   // O input faz esse comunicação entre componente pai e componente filho, nesse caso o componente pai terá as informações do componente filho e o input pega essas informações do componente pai para o componente filho. Ele cria uma propriedade para (nesse caso) o app card e podemos usa-lo usando o property binding
   @Input('planType') planType : string = ''; // criamo uma propriedade chamada planType que vai receber uma str. Temos também uma alias para o nome da propriedade, geralmente é bom deixar com o mesmo nome para não causar confusão. Esse valor ao lado do tipo é o valor padrão dessa propriedade.
   @Input({required: true}) planPrice : number = 0 ; // O required diz que essa propriedade terá que receber um valor do pai, então caso não tenha terá um erro de compilação.
 
+  clicksCounter(valorEmmitted: number){
+    this.valueEmitted = valorEmmitted;
+  }
 }
